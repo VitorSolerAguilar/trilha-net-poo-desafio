585c4115e5e790087b14c7c5c7a8617318fe21c3
VitorSolerAguilar <vitorsoleraguilar49@gmail.com>
2024-05-06 20:55:01 -0300

    Implementar nokia/iphone na classe main

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone(numero: "11 95636-5688", modelo: "Xr", imei: "11ADFR5", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Sudoku");
+
+Console.WriteLine("\n");
+
+Nokia nokia = new Nokia(numero: "15 55555-7777", modelo: "12 PRO", imei: "15LDPO", memoria: 1000);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
